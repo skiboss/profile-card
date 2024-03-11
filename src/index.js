561135967a367561b9415fe6744c242e595d62ae
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2024-03-11 17:12:33 +0100

    modify source code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skillSet = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#F0DB4F",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#61DBFB",
+  },
+  {
+    skill: "Bootstrap",
+    level: "intermediate",
+    color: "#563D7C",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="./IMG_6464.jpeg" alt="Avatar" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Oghenemeru Avwemoya</h1>
+      <p>
+        Frontend Developer and Technical Writer, passionate about solving
+        problems. When I'm not coding, you can find me swimming or cooking.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  const skills = skillSet;
+
+  return (
+    <ul className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} bgColor={skill.color} />
+      ))}
+      {/* <Skill skill="Writing" emoji="📝" bgColor="green" />
+      <Skill skill="JavaScript" emoji="🐱‍👤" bgColor="yellow" />
+      <Skill skill="React" emoji="⚛️" />
+      <Skill skill="Bootstrap" emoji="📦" bgColor="teal" /> */}
+    </ul>
+  );
+}
+
+function Skill({ skill, level, bgColor }) {
+  return (
+    <li>
+      <h3
+        className="skill"
+        style={
+          bgColor ? { backgroundColor: bgColor } : { backgroundColor: "blue" }
+        }
+      >
+        <span>{skill}</span>
+        <span>
+          {level === "advanced" && "🐱‍👤"}
+          {level === "intermediate" && "👍"}
+          {level === "beginner" && "😁"}
+        </span>
+      </h3>
+    </li>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
